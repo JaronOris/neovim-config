4697312f58dd5380cf661d702e4c7e0f7678ad5d
Jaron Oris <jaronoris@protonmail.com>
2025-06-09 15:34:31 -0400

    Add treesitter language support

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
   config = function ()
     local config = require ("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = {"lua"},
+      ensure_installed = {"angular", "bash", "comment", "css", "dockerfile", "editorconfig", "fish", "git_config", "git_rebase", "hjson", "html", "javascript", "json", "lua", "nix", "php", "python", "scss", "sql", "typescript", "xml", "yuck"},
       highlight = { enable = true },
       indent = { enabled = true },
     })
