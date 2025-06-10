da80e68238e5451f8ec1c29ada16f8742d25881a
Jaron Oris <jaronoris@protonmail.com>
2025-06-10 15:41:40 -0400

    Add LSP & telescope-ui-select

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -8,12 +8,18 @@ return {
     "mason-org/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls" }
+        ensure_installed = { "lua_ls", "ts_ls" }
       })
     end
   },
   {
     "neovim/nvim-lspconfig",
-    vim.lsp.enable('lua_ls')
+    config = function()
+      vim.lsp.enable('lua_ls')
+      vim.lsp.enable("ts_ls")
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      vim.keymap.set({ 'n' }, '<leader>ca', vim.lsp.buf.code_action, {})
+    end
   }
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,5 @@
-return {
-  'nvim-telescope/telescope.nvim', tag = '0.1.8',
+return { 
+  { 'nvim-telescope/telescope.nvim', tag = '0.1.8',
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function() 
     local builtin = require('telescope.builtin')
@@ -8,4 +8,19 @@ return {
     vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
   end
+},
+{
+  'nvim-telescope/telescope-ui-select.nvim',
+  config = function ()
+    require("telescope").setup {
+      extensions = {
+        ["ui-select"] = {
+          require("telescope.themes").get_dropdown {
+          }
+        }
+      }
+    }
+    require("telescope").load_extension("ui-select")
+  end
+},
 }
