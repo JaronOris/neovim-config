b0bfa5090e4e2c13bff1c3ea15ba2718de13fdd2
Jaron Oris <jaronoris@protonmail.com>
2025-06-16 12:41:40 -0400

    Add none-ls & update treesitter to auto install

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,12 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  config = function ()
-    local config = require ("nvim-treesitter.configs")
+  config = function()
+    local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = {"angular", "bash", "comment", "css", "dockerfile", "editorconfig", "fish", "git_config", "git_rebase", "hjson", "html", "javascript", "json", "lua", "nix", "php", "python", "scss", "sql", "typescript", "xml", "yuck"},
+      auto_install = true,
       highlight = { enable = true },
       indent = { enabled = true },
     })
-  end
+  end,
 }
